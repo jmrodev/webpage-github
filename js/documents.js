16a8f309deae40e9eafdd43b075cdd8e638d06e8
jmrodev <juanmarcelo.rodrigueztandil@gmail.com>
2025-03-26 14:40:19 -0300

    refactor: reorganize analysis pages and update links; remove unused scripts

--- js/documents.js
+++ js/documents.js
@@ -1,8 +1,6 @@
 function showIframe(iframeId) {
-    // Ocultar todos los iframes
     document.querySelectorAll('.iframe-pdf').forEach(iframe => {
         iframe.classList.add('hidden');
-    });
-    // Mostrar el iframe seleccionado
+    });    
     document.getElementById(iframeId).classList.remove('hidden');
 }
